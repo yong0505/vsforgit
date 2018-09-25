f3754f289d9be716fb4a560265334e5a93175e00
yong0505 <wangxiaoyong05@163.com>
2018-09-25 10:10:13 +0800

    模块 test2 的初始签入

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace test2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
